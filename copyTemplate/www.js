c79596780e807384e3d37bffed153444a0eb02f9
jerry <936333511@qq.com>
2016-05-16 15:22:42 +0800

    add replace content method

--- copyTemplate/www.js
+++ copyTemplate/www.js
@@ -1,6 +1,5 @@
 //自动完成从gitHub下载对应的module 并替换成变量
 var copyTemplate  = require('./copyTemplate');
-//copyTemplate.downloadFileByCurl('todo');
-copyTemplate.unzipTemplate();
+copyTemplate.downloadFileByCurl('todo');
 
 
